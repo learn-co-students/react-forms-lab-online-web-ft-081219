4ea474c7009c4faca26a4182649565e08a2d5a99
Armel <armelfli50@gmail.com>
2019-12-18 12:01:54 -0600

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,35 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {username: "" ,password: ""};
+  }
+
+  handleLogin = event=> {
+    console.log(event)
+      if(this.state.username !== "" && this.state.password !== ""){
+        this.props.handleLogin(this.state)
+
+      }
+    event.preventDefault()
+  }
+
+  handleChange = event => {
+    this.setState({[event.target.name]: event.target.value})
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleLogin}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input value ={this.state.username} onChange={this.handleChange} id="username" name="username" type="text" />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input value ={this.state.password} onChange={this.handleChange} id="password" name="password" type="password" />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,19 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {tweet: "", maxChars: 280};
+  }
+  handleTweet = (event)=>{
+    
+    this.setState(previousState=>{return ({tweet: event.target.value, maxChars: previousState.maxChars - event.target.value.length  }) })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <h6>You have {this.state.maxChars} characters remainings</h6>
+        <input onChange={this.handleTweet} type="text" name="message" id="message" value={this.state.tweet} />
       </div>
     );
   }
